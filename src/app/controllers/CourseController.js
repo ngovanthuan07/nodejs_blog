40c4939d520a3b77a46cc6a4beffcb2f445cdc6e
Thuan Dev <ngovanthuan07@gmail.com>
2022-06-25 22:02:37 +0700

    select all with checkbox

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -62,7 +62,21 @@ class CourseController {
         .catch(next); 
     }
 
+    // [POST]  /courses/handle-form-action
+    handleFormActions(req, res, next) {
 
+        switch(req.body.action) {
+            case 'delete': {
+                Course.delete({_id: { $in: req.body['courseIds[]']}})
+                    .then(() => res.redirect('back'))
+                    .catch(next);
+                
+                break;
+            }
+            default:
+                res.json({message: 'Action is invalid'});
+        }
+    }
     
 
 }
